707b665cfa7238027be214a59b73fc1576a025a8
HeidiSiao <heidi.siao@gmail.com>
2025-06-11 20:18:21 +0800

    feat: add getAllProfiles API  for demo matching pool

--- src/routes/userProfileRoutes.js
+++ src/routes/userProfileRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getProfileById } = require("../controllers/userProfileControllers");
+const {
+  getProfileById,
+  getAllProfiles,
+} = require("../controllers/userProfileControllers");
 
 router.get("/profile/:userId", getProfileById);
-
+router.get("/profiles", getAllProfiles);
 module.exports = router;
