8870a5096922d6aa72723fc14bc93182717eeb73
Sin huang <melody880306@gmail.com>
2025-06-13 14:01:38 +0800

    feat: implement to update the inventory of product from admin request

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,6 +1,7 @@
 const { uploadFileToS3 } = require("../services/s3Service.js");
 const db = require("../db/index.js");
 const { productsTable } = require("../db/schema.js");
+const { eq } = require("drizzle-orm");
 
 // 上傳圖片
 async function uploadProductImage(req, res) {
@@ -37,7 +38,9 @@ async function createProduct(req, res) {
 // 取得所有商品
 async function getAllProducts(req, res) {
   try {
-    const products = await db.select().from(productsTable);
+    // 因為 postgresql 預設查詢結果 不保證順序性 但我要商品依照 id 排序 
+    // 所以加上 orderBy
+    const products = await db.select().from(productsTable).orderBy(productsTable.id);
     res.json(products);
   } catch (err) {
     console.error("讀取商品失敗", err);
@@ -45,4 +48,27 @@ async function getAllProducts(req, res) {
   }
 }
 
-module.exports = { uploadProductImage, createProduct, getAllProducts };
+// 0613 黃馨
+async function updateProductsInventory(req,res){
+  try{
+    const productId = parseInt(req.params.id);
+    const { inventory } = req.body;
+
+    if( isNaN(productId) || typeof inventory !== "number"){
+      return res.status(400).json({ error: "參數錯誤"});
+    }
+
+    try{
+      await db.update(productsTable).set({ inventory}).where(eq(productsTable.id, productId));
+      
+      res.json({success:true, message:"庫存已更新"});
+    }catch(err){
+      console.err("庫存更新失敗原因：", err);
+      req.status(500).json({error: "庫存更新失敗"});
+    }
+  }catch(err){
+
+  }
+}
+
+module.exports = { uploadProductImage, createProduct, getAllProducts, updateProductsInventory };
